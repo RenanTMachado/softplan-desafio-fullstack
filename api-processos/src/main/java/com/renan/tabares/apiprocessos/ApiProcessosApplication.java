bc78a722cefef73826b17eee2ce5a773ee74dfc6
RenanTMachado <renantabares@restinga.ifrs.edu.br>
2019-08-09 23:15:41 -0300

    projeto criado

---
+++ api-processos/src/main/java/com/renan/tabares/apiprocessos/ApiProcessosApplication.java
@@ -0,0 +1,13 @@
+package com.renan.tabares.apiprocessos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProcessosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProcessosApplication.class, args);
+	}
+
+}
